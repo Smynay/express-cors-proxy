42176aa8054f784d54a9993f771b0e0ae1fa2ce0
Yerzhan Utelbayev <yerzhan.utelbayev@gmail.com>
2020-12-01 15:17:59 +0600

    feat: add logging to the project

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const morgan = require('morgan');
+const morganBody = require('morgan-body');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const {createProxyMiddleware} = require('http-proxy-middleware');
@@ -10,6 +12,14 @@ dotenv.config();
 const app = express();
 
 app.use(cors());
+app.use(morgan('dev'));
+morganBody(app);
+
+app.use('/', (req, res, next) => {
+  console.dir(req.headers);
+  next();
+});
+
 app.use('/api', createProxyMiddleware({
   target: TARGET,
   changeOrigin: true,
